b24f275106ac65af389a1eff1c256c8854e205a2
Akram Sabbah <akramsabbah9@gmail.com>
2021-02-19 21:19:52 -0800

    minor bugfix to frontend, added nodemon as a devDependency, finished working POST requests

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 /* IMPORT MODULES */
+const fs = require("fs");
 const path = require("path");
 const express = require("express");
 const notes = require("./db/db.json"); // notes from db directory
@@ -10,10 +11,43 @@ const app = express();
 // if port isn't an environment variable, set it to 3001
 const PORT = process.env.PORT || 3001;
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(express.static("public"));
 
 
 /* FUNCTIONS */
+// if the note is missing a title or text attribute, return false.
+const validateNote = note => {
+    if (!note.title || typeof note.title !== "string") {
+        console.log("no title!");
+        return false;
+    }
+    if (!note.text || typeof note.text !== "string") {
+        console.log("no text!");
+        return false;
+    }
+    return true;
+};
+
+// add a new note to the noteArray.
+const createNewNote = (body, notesArray) => {
+    // set id to next possible index
+    let newNote = body;
+    newNote.id = notesArray.length.toString();
+    
+    // push note to notes array
+    notesArray.push(newNote);
+    
+    // write the updated notes array to db.json
+    fs.writeFileSync(
+        path.join(__dirname, "./db/db.json"),
+        JSON.stringify(notesArray, null, 4)
+    );
+
+    // return finished data to the POST route for response
+    return newNote;
+};
 
 
 /* ROUTING */
@@ -34,9 +68,18 @@ app.get("/api/notes", (req, res) => {
     res.json(notes);
 });
 
-// TODO: add new note to db.json from POST body
+// add new note to db.json from POST body
 app.post("/api/notes", (req, res) => {
-    ;
+    console.log(req.body);
+    // if any data in req.body is incorrect, send 400 error
+    if (!validateNote(req.body)) {
+        res.status(400).send('The note is not properly formatted.');
+    }
+    // push the user-created note to notes, and send the new note back.
+    else {
+        const note = createNewNote(req.body, notes);
+        res.json(note);
+    }
 });
 
 /* MAIN */
