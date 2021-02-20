3373614d9e31ec24345270bcba57595f9bf3da1c
Akram Sabbah <akramsabbah9@gmail.com>
2021-02-20 15:17:29 -0800

    refactored code to stop depending on a local version of the notes array to work

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,14 +1,14 @@
 /* apiRoutes/index.js: Contains all api endpoints to get and create notes */
 const router = require("express").Router();
-const { validateNote, createNewNote } = require("../../lib/notes");
+const { readNotes, validateNote, createNewNote } = require("../../lib/notes");
 const notes = require("../../db/db"); // notes from db directory
 
-// read notes from db.json and send to user
+// read notes from db.json and send to the user.
 router.get("/notes", (req, res) => {
-    res.json(notes);
+    res.json(readNotes());
 });
 
-// add new note to notes array and db.json from POST body
+// add new note to the notes array in db.json from the POST body.
 router.post("/notes", (req, res) => {
     // if any data in req.body is incorrect, send 400 error
     if (!validateNote(req.body)) {
@@ -16,21 +16,26 @@ router.post("/notes", (req, res) => {
     }
     // push the user-created note to notes, and send the new note back.
     else {
-        const note = createNewNote(req.body, notes);
+        const note = createNewNote(req.body);
         res.json(note);
     }
 });
 
-// TODO: delete note from notes array and db.json (TODO: need id npm package first)
+// delete note from the notes array in db.json.
 router.delete("/notes/:id", (req, res) => {
+    // 1) Update the local version of notes
+    // try to find the index of the targeted note by checking all the existing ids.
+    targetIndex = notes.findIndex(ele => ele.id === req.params.id);
+
     // if id is valid, delete corresponding note from notes array
-    // notes.splice(req.params.id, 1);
+    notes.splice(targetIndex, 1);
     
     // after deleting, write new array to db.json and return 
-    /* fs.writeFileSync(
-        path.join(__dirname, "../../db/db.json"),
-        JSON.stringify(notes, null, 4)
-    );*/
+    // fs.writeFileSync(
+    //     path.join(__dirname, "../../db/db.json"),
+    //     JSON.stringify(notes, null, 4)
+    // );
+
     res.send(`deleted note with id: ${req.params.id}.`);
 });
 
